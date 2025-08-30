8589d94ad46c0ebb6395921594b4229a8781cfdc
akashtrustgrid <akasharun@trustgrid.com>
2025-08-30 19:14:29 +0530

    Akash J: Feed and pending request APIs implemented

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middleware/userAuth");
 const ConnectionRequest = require("../models/connectionRequests");
+const User = require("../models/user");
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -15,6 +16,21 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     res.json({ message: "Connection requests", data: connectionRequests });
   } catch (error) {
     res.status(401).send("ERROR : " + error.message);
+  }
+});
+
+userRouter.get("/user/requests/pending", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      fromUserId: loggedInUser._id,
+      status: "intrested",
+    }).populate("toUserId", ["firstName", "lastName"]);
+
+    res.json({ message: "Connection requests", data: connectionRequests });
+  } catch (error) {
+    res.status(400).send("ERROR : " + error.message);
   }
 });
 
@@ -41,6 +57,38 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   } catch (error) {
     res.status(401).send("ERROR : " + error.message);
   }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  const page = req.query.page || 1;
+  let limit = req.query.limit || 10;
+  const skip = (page - 1) * limit;
+
+  limit = limit > 20 ? 20 : limit;
+
+  const loggedInUser = req.user;
+
+  const connectionRequests = await ConnectionRequest.find({
+    $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+  });
+
+  const notAllowedUsers = new Set();
+
+  connectionRequests.forEach((conn) => {
+    notAllowedUsers.add(conn.toUserId);
+    notAllowedUsers.add(conn.fromUserId);
+  });
+
+  const users = await User.find({
+    $and: [
+      { _id: { $ne: loggedInUser._id } },
+      { _id: { $nin: [...notAllowedUsers] } },
+    ],
+  })
+    .select(["firstName", "lastName"])
+    .skip(skip)
+    .limit(limit);
+  res.json({ message: "Feeds", data: users });
 });
 
 module.exports = userRouter;
